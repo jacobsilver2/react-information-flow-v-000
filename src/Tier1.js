0a6c6ac9cd475fa8fbcdd5db90b176154e78ae98
jacobsilver2 <jacobsilver2@mac.com>
2018-06-11 09:52:04 -0400

    third

--- src/Tier1.js
+++ src/Tier1.js
@@ -11,17 +11,59 @@ export default class Tier1 extends Component {
     this.state = {
       color: initialColor,
       childColor: getReducedColor(initialColor)
+      grandchildColor: 
     }
   }
 
+  generateColors = (count=3) => {
+    const colors = [getRandomColor()]
+    
+    for (i=0; i<count; i++) {
+      colors.push(getReducedColor(colors[colors.length -1]))
+    }
+    return colors
+  }
+
+  handleClick = () => {
+    const [c1, c2, c3] = this.generateColors()
+    this.setState({
+      color: c1,
+      childColor: c2,
+      grandchildColor: c3
+    })
+  }
+
+  handleChildClick = (e) => {
+    e.stopPropagation()
+    const [c2, c3] = this.generateColors(2)
+    this.setState({
+      childColor: c2,
+      grandchildColor: c3
+    })
+  }
+
+  handleGrandchildClick = (e) => {
+    e.stopPropagation()
+    const [c3] = this.generateColors(1)
+    this.setState({
+      grandchildColor: c3
+    })
+  }
+
+
   render() {
-    // hard coded color values have been added below, though they won't be
-    // present in our solution. What should they be replaced with?
     return (
-      <div onClick={() => {this.setState({color: getRandomColor(), childColor: getReducedColor(this.state.color)})}} className="tier1" style={{backgroundColor: this.state.color, color: this.state.color}}>
+      <div onClick={this.handleClick} className="tier1" style={{backgroundColor: this.state.color, color: this.state.color}}>
         <Tier2 color={this.state.childColor} />
         <Tier2 color={this.state.childColor} />
       </div>
     )
   }
 }
+
+
+
+
+
+
+
